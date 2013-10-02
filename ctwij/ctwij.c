83822e2e407939f2a01a2d3dfd98a38cb5f94aaa
glts <676c7473@gmail.com>
2013-10-02 21:54:00 +0200

    Added problems 1, 2, 3 from JfC in C and J

--- ctwij/ctwij.c
+++ ctwij/ctwij.c
@@ -23,6 +23,38 @@ int hoursworked[NEMP][31] = {
     {9, 0, 0, 9, 10, 8, 6, 6, 8, 0, 9, 8, 10, 6, 9, 7, 0, 6, 8, 8, 8, 9, 0, 5, 8, 9, 8, 8, 12, 0, 0},
 };
 
+/* Finds the number of hours each employee worked in the given month. */
+void emphours(int hrs[]) {
+    int i, j;
+    for (i = 0; i < NEMP; ++i) {
+        for (j = 0; j < 31; ++j) {
+            hrs[i] += hoursworked[i][j];
+        }
+    }
+}
+
+/* Determines the wages earned by each employee in the given month. */
+void empearnings(int earns[]) {
+    int i, j;
+    for (i = 0; i < NEMP; ++i) {
+        for (j = 0; j < 31; ++j) {
+            earns[i] += hoursworked[i][j];
+        }
+        earns[i] *= payrate[i];
+    }
+}
+
+/* Determines the profit brought in by each employee. */
+void empprofit(int profit[]) {
+    int i, j, temp;
+    for (i = 0; i < NEMP; ++i) {
+        for (j = 0, temp = 0; j < 31; ++j) {
+            temp += hoursworked[i][j];
+        }
+        profit[i] = temp * (billrate[i] - payrate[i]);
+    }
+}
+
 /* Returns the amount to bill a given client. */
 int billclient(int cno)
 {
@@ -57,6 +89,30 @@ void dailydrudge(int drudges[31])
 int main(int argc, char *argv[])
 {
     int i;
+
+    /* Problem 1 */
+    int hrs[NEMP];
+    emphours(hrs);
+    for (i = 0; i < NEMP-1; i++) {
+        printf("%d ", hrs[i]);
+    }
+    printf("%d\n", hrs[NEMP-1]);
+
+    /* Problem 2 */
+    int earns[NEMP];
+    empearnings(earns);
+    for (i = 0; i < NEMP-1; i++) {
+        printf("%d ", earns[i]);
+    }
+    printf("%d\n", earns[NEMP-1]);
+
+    /* Problem 3 */
+    int profit[NEMP];
+    empprofit(profit);
+    for (i = 0; i < NEMP-1; i++) {
+        printf("%d ", profit[i]);
+    }
+    printf("%d\n", profit[NEMP-1]);
 
     /* Problem 4 */
     int clientno = 10025;
